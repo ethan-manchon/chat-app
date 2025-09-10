038cbdf782b22758157118f67aa094dbedc94550
Ethan <ethan.manchon@etu.unilim.fr>
2025-09-10 10:29:59 +0200

    render

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { Server } from "socket.io";
 import twig from 'twig';
 import { fileURLToPath } from 'url';
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const prisma = new PrismaClient()
 const __filename = fileURLToPath(import.meta.url);
@@ -71,7 +73,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
